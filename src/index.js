4ebc17665fb9080c19a79eaa17d0740e61869b32
CodeMaestro <CodeMaestro@outlook.com>
2020-04-14 18:47:08 +0100

    Changed logging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-
 import xmlparser from 'express-xml-bodyparser';
 import xml from 'xml2js';
 import covid19ImpactEstimator from './estimator';
@@ -33,11 +32,13 @@ const builder = new xml.Builder({
   renderOpts: { pretty: false }
 });
 
+let requestTitle;
+let httpAction;
+
 app.post('/api/v1/on-covid-19', xmlparser(xmlOptions), (req, res) => {
   const startHrTime = process.hrtime();
   const reqParams = req.query;
   const result = covid19ImpactEstimator(req.body);
-  let requestTitle;
 
   if (reqParams.hasXML) {
     res.set('Content-Type', 'application/xml');
@@ -50,35 +51,41 @@ app.post('/api/v1/on-covid-19', xmlparser(xmlOptions), (req, res) => {
         }
       })
     );
+  } else if (reqParams.hasJson) {
+    res.status(200).send({
+      data: req.body,
+      impact: result.impact,
+      severeImpact: result.severeImpact
+    });
   } else {
+    httpAction = 'POST';
+    requestTitle = 'on-covid-19';
     res.status(200).send({
       data: req.body,
       impact: result.impact,
       severeImpact: result.severeImpact
     });
   }
-
-  if (reqParams.title === 'xml') {
-    requestTitle = 'on-covid-19/xml';
-  } else if (reqParams.title === 'json') {
-    requestTitle = 'on-covid-19/json';
-  } else {
-    requestTitle = 'on-covid-19';
-  }
   const elapsedHrTime = process.hrtime(startHrTime);
   const elapsedTimeInMs = (elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6).toFixed(2);
-  log.info(`${Date.now()} \t\t${requestTitle}  \t\tdone in ${elapsedTimeInMs} miliseconds`);
+  log.info(`${httpAction} \t\t${Date.now()} \t\t${requestTitle} \t\t${elapsedTimeInMs}ms`);
 });
 
 app.post('/api/v1/on-covid-19/xml', (req, res) => {
-  res.redirect(307, '/api/v1/on-covid-19?title=xml&hasXML=true');
+  httpAction = 'GET';
+  requestTitle = 'on-covid-19/xml';
+  res.redirect(307, '/api/v1/on-covid-19?hasXML=true');
 });
 
 app.post('/api/v1/on-covid-19/json', (req, res) => {
-  res.redirect(307, '/api/v1/on-covid-19?title=json');
+  httpAction = 'GET';
+  requestTitle = 'on-covid-19/json';
+  res.redirect(307, '/api/v1/on-covid-19?hasJson=true');
 });
 
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
+  httpAction = 'GET';
+  requestTitle = 'on-covid-19/logs';
   const startHrTime = process.hrtime();
   fs.readFile('project.log', 'utf8', (err, data) => {
     if (err) throw err;
@@ -87,7 +94,7 @@ app.get('/api/v1/on-covid-19/logs', (req, res) => {
   });
   const elapsedHrTime = process.hrtime(startHrTime);
   const elapsedTimeInMs = (elapsedHrTime[0] * 1000 + elapsedHrTime[1] / 1e6).toFixed(2);
-  log.info(`${Date.now()} \t\ton-covid-19/logs \t\tdone in ${elapsedTimeInMs} miliseconds`);
+  log.info(`${httpAction} \t\t${Date.now()} \t\t${requestTitle} \t\t${elapsedTimeInMs}ms`);
 });
 
 const PORT = process.env.PORT || 1337;
